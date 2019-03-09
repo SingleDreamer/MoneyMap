b274f40976be126b513acc8cfb6a2ff20e95bce9
Cristopher Jimenez <crisjimenez120@gmail.com>
2019-03-09 17:34:53 -0500

    fixed small bug

--- moneymap-client/src/Components/CardArray/CardArray.js
+++ moneymap-client/src/Components/CardArray/CardArray.js
@@ -12,11 +12,38 @@ class CardArray extends Component {
 
   //Whenever the CardArray component receives new props from the Dashboard
   //it will rerender with the new list of Cards
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      companies: nextProps.companies
-    });
-  }
+  // componentWillReceiveProps(nextProps) {
+  //   this.setState({
+  //     companies: nextProps.companies
+  //   });
+
+  //   async sendRequest(() => {
+  //     let url = "http://ec2-18-217-169-247.us-east-2.compute.amazonaws.com:3000/users/11111111-1111-1111-1111-111111111111/jocs";
+
+  //     let config = {
+  //       headers: {
+  //         // Authorization: this.Auth.getToken(),
+  //         "Content-Type": "application/json"
+  //       }
+  //     };
+
+
+
+  //     try {
+  //       let response = await axios.post(url, config);
+  //       console.log("****");
+  //       console.log(response);
+  //       // if (response.data.message) alert(response.data.message);
+  //       return response;
+  //     } catch (err) {
+  //       this.setState({ error: err });
+  //       console.log("####");
+  //       console.log(err);
+  //       // alert(`Ya got an error boy \n
+  //       // ${err}`);
+  //   }
+  // })
+  // }
 
   render() {
     let cards = this.state.companies.cards.map((company, index) => (
