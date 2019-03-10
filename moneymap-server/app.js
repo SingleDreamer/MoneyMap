6a0bbcd5633178b01585a93dc9b7932687052e16
Ubuntu <ubuntu@ip-172-31-17-207.us-east-2.compute.internal>
2019-03-10 00:07:31 +0000

    changed location of cors initialization

--- moneymap-server/app.js
+++ moneymap-server/app.js
@@ -12,6 +12,7 @@ var citiesRouter = require('./routes/cities');
 var cardsRouter = require('./routes/cards');
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,7 +23,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
