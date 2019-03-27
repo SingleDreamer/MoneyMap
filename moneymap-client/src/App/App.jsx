fd81afdbbb93f69cb02ae9a70ce28c62ac18b50b
Eunice Hew <eunicehew97@gmail.com>
2019-03-27 18:42:40 -0400

    auth

--- moneymap-client/src/App/App.jsx
+++ moneymap-client/src/App/App.jsx
@@ -1,21 +1,18 @@
 import React, { Component } from "react";
 import Home from "../Home/Home.jsx";
 import Dashboard from "../Dashboard/Dashboard.jsx";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import withAuth from "../Components/AuthService/WithAuth.js";
 class App extends Component {
   render() {
     return (
-      <div className = 'App'>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/dashboard" component={Dashboard} />
-        </Switch>
-      </Router>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/dashboard" component={withAuth(Dashboard)} />
+          </Switch>
+        </Router>
       </div>
     );
   }
