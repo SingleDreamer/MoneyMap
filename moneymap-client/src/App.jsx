94a7280b4bf336f9aba7f45c1ea88b59a687cd71
Cristopher Jimenez <crisjimenez120@gmail.com>
2019-04-14 20:00:47 -0400

    tried changing themes

---
+++ moneymap-client/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Home from "./Home/Home.jsx";
+import Dashboard from "./Dashboard/Dashboard.jsx";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+//import withAuth from "../Components/AuthService/WithAuth.js";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            {/* <Route exact path="/dashboard" component={withAuth(Dashboard)} /> */}
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
