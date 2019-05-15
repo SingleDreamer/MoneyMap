a0ddf7050d6d2a4104b1a5dfd8a52cefdf68967a
Riyadh Rahman <8844262+laplaceon@users.noreply.github.com>
2019-05-15 16:49:24 -0400

    Moved tax code to service

--- moneymap-server/scripts/taxtest.js
+++ moneymap-server/scripts/taxtest.js
@@ -4,35 +4,50 @@ var db = require("../services/db")
 const request = require('request-promise-native');
 
 request("https://google.com").then(async function(unused) {
-  const request = new sql.Request(db);
-  // request.input('uid', sql.UniqueIdentifier, "599DAFEE-D337-4DC3-8F83-B932B4837AB6");
-  // request.input('token', sql.UniqueIdentifier, "204EB0A8-1914-4CDE-8686-32A8D1C4230B");
-  request.input('cityid', sql.Int, "10107");
+  let dbrequest = new sql.Request(db);
+  dbrequest.input('cityid', sql.Int, "10107");
 
-  let result = await request.execute('sp_get_city');
+  let result = await dbrequest.execute('sp_get_city');
 
   let region = result.recordset[0];
 
-  console.log();
+  // Only calculate taxes if in US
+  if(region.Country == "United States") {
+    let state = region.City.substring(region.City.length - 2);
 
-  request({
-    method: "POST",
-    url: "https://taxee.io/api/v2/calculate/2019",
-    headers: {
-      "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBUElfS0VZX01BTkFHRVIiLCJodHRwOi8vdGF4ZWUuaW8vdXNlcl9pZCI6IjVjZDEyMTRkNGU5ZjIxNTMzNDQyZjBmOCIsImh0dHA6Ly90YXhlZS5pby9zY29wZXMiOlsiYXBpIl0sImlhdCI6MTU1NzIwOTQyMX0.R1PxmgK0t8Z9UCHJsIGVdtb8pzofaCC2H_I3wWhqbvU",
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    form: {
-      "state": "NC",
-      "filing_status": "married",
-      "pay_periods": 26,
-      "pay_rate": 116500,
-      "exemptions": 2
-    }
-  }).then(function(res) {
-    console.log(res);
-  }).catch(function(err) {
-    console.log(err);
-  });
+    let dbrequest = new sql.Request(db);
+
+    dbrequest.input('uid', sql.UniqueIdentifier, "67E2CD40-1D16-43A2-B0DE-0E0C67598AF0");
+    dbrequest.input('token', sql.UniqueIdentifier, "BC7679DA-F682-4897-8BEF-3A9258DC1442");
+
+    let result = await dbrequest.execute('sp_get_tax_info');
+
+    request({
+      method: "POST",
+      url: "https://taxee.io/api/v2/calculate/2019",
+      headers: {
+        "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBUElfS0VZX01BTkFHRVIiLCJodHRwOi8vdGF4ZWUuaW8vdXNlcl9pZCI6IjVjZDEyMTRkNGU5ZjIxNTMzNDQyZjBmOCIsImh0dHA6Ly90YXhlZS5pby9zY29wZXMiOlsiYXBpIl0sImlhdCI6MTU1NzIwOTQyMX0.R1PxmgK0t8Z9UCHJsIGVdtb8pzofaCC2H_I3wWhqbvU",
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      form: {
+        "state": state,
+        "filing_status": result.recordset[0].filing_status,
+        "pay_rate": 116500,
+        "exemptions": result.recordset[0].exemptions
+      }
+    }).then(function(res) {
+      let taxes = JSON.parse(res).annual;
+
+      let totalTaxAmount = taxes.fica.amount + taxes.federal.amount;
+      if(taxes.state.amount != null) {
+        totalTaxAmount += taxes.state.amount;
+      }
+      console.log(totalTaxAmount);
+    }).catch(function(err) {
+      console.log(err);
+    });
+  }
+
+
 
 });
