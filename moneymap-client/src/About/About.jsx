d5438c8793a5c364e6820e706302eaa66a3f918f
Cristopher Jimenez <crisjimenez120@gmail.com>
2019-05-19 16:23:29 -0400

    change about icon

--- moneymap-client/src/About/About.jsx
+++ moneymap-client/src/About/About.jsx
@@ -15,7 +15,7 @@ class About extends Component {
     this.state = {
       showRegister: false,
       showLogin: false,
-      open: false,
+      open: false
     };
     this.Auth = new AuthService();
   }
@@ -29,83 +29,126 @@ class About extends Component {
     this.props.history.replace("/Dashboard");
   };
 
-
   render() {
     const { open } = this.state;
     let navbarButton = "";
     let navbarDash = "";
     if (this.Auth.loggedIn()) {
-      navbarButton = <Button variant="danger" onClick={this.clearSession} style={{marginRight:"10px"}}>
-        Log Out
-      </Button>
-      navbarDash = <Nav.Link href="/Dashboard">Dashboard</Nav.Link>
-
+      navbarButton = (
+        <Button
+          variant="danger"
+          onClick={this.clearSession}
+          style={{ marginRight: "10px" }}
+        >
+          Log Out
+        </Button>
+      );
+      navbarDash = <Nav.Link href="/Dashboard">Dashboard</Nav.Link>;
     } else {
-      navbarButton = <Button variant="info" onClick={this.redirectToDashboard} style={{marginRight:"10px"}}> Log in </Button>
+      navbarButton = (
+        <Button
+          variant="info"
+          onClick={this.redirectToDashboard}
+          style={{ marginRight: "10px" }}
+        >
+          {" "}
+          Log in{" "}
+        </Button>
+      );
     }
     return (
       <div className="about">
-      <Navbar variant="dark" className="justify-content-between">
-        <Navbar.Brand>
-        <i
-        className="fas fa-map-signs navIcon d-inline-block align-top"
-        onClick={this.toggleDrawer}
-        width="30"
-        height="30"
-        alt = ""/>
-        <a className="title">{ 'MoneyMap' }</a>
-        </Navbar.Brand>
-        <Nav className="mr-auto">
-          {navbarDash}
-          <Nav.Link href="/About">About</Nav.Link>
-        </Nav>
-        <Nav>
-          <Nav.Link>
-            {navbarButton}
-          </Nav.Link>
-        </Nav>
-      </Navbar>
+        <Navbar variant="dark" className="justify-content-between">
+          <Navbar.Brand>
+            <div className="title">
+              <i className="fas fa-map-signs navIcon" />
+              {"MoneyMap"}
+            </div>
+          </Navbar.Brand>
+          <Nav className="mr-auto">
+            {navbarDash}
+            <Nav.Link href="/About">About</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link>{navbarButton}</Nav.Link>
+          </Nav>
+        </Navbar>
 
-      <div>
-      <h1 className="pagetitle">About Page</h1>
-      </div>
+        <div>
+          <h1 className="pagetitle">About Page</h1>
+        </div>
 
-      <div style = {{marginTop:"90px", marginRight:"200px", marginLeft:"200px"}}>
-      <p className="heading" id="RFS">RFS</p>
-      <p className="text">The Relative Financial Score (RFS) is MoneyMap’s unique performance score that shows the financial standing of the Job Offer Card relative to averages in the city,
-      that is, how “good” this job offer is for the city it is being offered in,
-      based on the user’s expenses, savings, the job offer and the city’s cost of living.
-      </p>
-      <p className="text">
-      Based on our current equation, a good score would then be somewhere in the range of 30-50 and a great score would be anything larger.
-      Anything lower than 30 is considered unsustainable.
-      </p>
-      </div>
+        <div
+          style={{
+            marginTop: "90px",
+            marginRight: "200px",
+            marginLeft: "200px"
+          }}
+        >
+          <p className="heading" id="RFS">
+            RFS
+          </p>
+          <p className="text">
+            The Relative Financial Score (RFS) is MoneyMap’s unique performance
+            score that shows the financial standing of the Job Offer Card
+            relative to averages in the city, that is, how “good” this job offer
+            is for the city it is being offered in, based on the user’s
+            expenses, savings, the job offer and the city’s cost of living.
+          </p>
+          <p className="text">
+            Based on our current equation, a good score would then be somewhere
+            in the range of 30-50 and a great score would be anything larger.
+            Anything lower than 30 is considered unsustainable.
+          </p>
+        </div>
 
-      <div style = {{marginTop:"90px", marginRight:"200px", marginLeft:"200px"}}>
-      <p className="heading" id="RFS">Basket of Goods</p>
-      <p className="text">The Relative Financial Score (RFS) is MoneyMap’s unique performance score that shows the financial standing of the Job Offer Card relative to averages in the city,
-      that is, how “good” this job offer is for the city it is being offered in,
-      based on the user’s expenses, savings, the job offer and the city’s cost of living.
-      </p>
-      <p className="text">
-      Based on our current equation, a good score would then be somewhere in the range of 30-50 and a great score would be anything larger.
-      Anything lower than 30 is considered unsustainable.
-      </p>
-      </div>
+        <div
+          style={{
+            marginTop: "90px",
+            marginRight: "200px",
+            marginLeft: "200px"
+          }}
+        >
+          <p className="heading" id="RFS">
+            Basket of Goods
+          </p>
+          <p className="text">
+            The Relative Financial Score (RFS) is MoneyMap’s unique performance
+            score that shows the financial standing of the Job Offer Card
+            relative to averages in the city, that is, how “good” this job offer
+            is for the city it is being offered in, based on the user’s
+            expenses, savings, the job offer and the city’s cost of living.
+          </p>
+          <p className="text">
+            Based on our current equation, a good score would then be somewhere
+            in the range of 30-50 and a great score would be anything larger.
+            Anything lower than 30 is considered unsustainable.
+          </p>
+        </div>
 
-      <div style = {{marginTop:"90px", marginRight:"200px", marginLeft:"200px"}}>
-      <p className="heading" id="UserDetails">User Details</p>
-      <p className="text">The Relative Financial Score (RFS) is MoneyMap’s unique performance score that shows the financial standing of the Job Offer Card relative to averages in the city,
-      that is, how “good” this job offer is for the city it is being offered in,
-      based on the user’s expenses, savings, the job offer and the city’s cost of living.
-      </p>
-      <p className="text">
-      Based on our current equation, a good score would then be somewhere in the range of 30-50 and a great score would be anything larger.
-      Anything lower than 30 is considered unsustainable.
-      </p>
-      </div>
-      
+        <div
+          style={{
+            marginTop: "90px",
+            marginRight: "200px",
+            marginLeft: "200px"
+          }}
+        >
+          <p className="heading" id="UserDetails">
+            User Details
+          </p>
+          <p className="text">
+            The Relative Financial Score (RFS) is MoneyMap’s unique performance
+            score that shows the financial standing of the Job Offer Card
+            relative to averages in the city, that is, how “good” this job offer
+            is for the city it is being offered in, based on the user’s
+            expenses, savings, the job offer and the city’s cost of living.
+          </p>
+          <p className="text">
+            Based on our current equation, a good score would then be somewhere
+            in the range of 30-50 and a great score would be anything larger.
+            Anything lower than 30 is considered unsustainable.
+          </p>
+        </div>
       </div>
     );
   }
